4b02afba6f351ab331205274be128887f35bcaf7
João Vítor G Douverny <douvernyjoaovitor@gmail.com>
2022-02-09 10:35:02 -0300

    Hora do dia

---
+++ script.js
@@ -0,0 +1,24 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 19
+    //msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora <= 12){
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+        msg.innerHTML = `Agora são ${hora} horas. Tenha um bom dia`
+
+    }else if(hora >= 13 && hora <= 18){
+        img.src = 'fototarde.png'
+        document.body.style.background = '#b9846f'
+        msg.innerHTML = `Agora são ${hora} horas. Tenha uma boa tarde`
+
+    }else{
+        img.src ='fotonoite.png' 
+        document.body.style.background = '#515154'
+        msg.innerHTML = `Agora são ${hora} horas. tenha uma boa noite`
+
+    }
+}
